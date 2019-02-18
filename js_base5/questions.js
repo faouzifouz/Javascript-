5057f3959f27b31c01c0cbd96e74e158e08c97b9
faouzifouz <titahfaouzi@gmail.com>
2019-02-18 16:49:17 +0100

    update

--- js_base5/questions.js
+++ js_base5/questions.js
@@ -36,33 +36,39 @@ var IsString = function (texte) {
 }
 
 var AfficherExtensionString = function (texte) {
-    return lastIndexOf(texte);
+    return texte.substr(texte.lastIndexOf('.') + 1);
 }
 var NombreEspaceString = function (texte) {
-
+    return texte.split(' ').length - 1;
 }
 var InverseString = function (texte) {
-
+    return texte.split('').reverse().join('');
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-
+    return Math.pow(x,y);
 }
 var valeurAbsolue = function (nombre) {
-
+return Math.abs(nombre);
 }
 var valeurAbsolueArray = function (array) {
-
+    for (i=0; i<array.length; i++){
+        array[i]= Math.abs(array[i]);
+    }
+    return array;
 }
 var sufaceCercle = function (rayon) {
-
+    return Math.round  
+      (Math.PI*(Math.pow(rayon,2)));
+    
 }
 var hypothenuse = function (ab, ac) {
-
+    return Math.sqrt(Math.pow(ab,2)+ Math.pow(ac,2));
 }
 var calculIMC = function (poids, taille) {
-
+    tailleSquare=Math.pow(taille,2);
+    return Number(((poids)/(tailleSquare)).toFixed(2));
 }
