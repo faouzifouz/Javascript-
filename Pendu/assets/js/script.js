8ffdc053a9355e814359a8cb554d5cdb17bf9366
faouzifouz <titahfaouzi@gmail.com>
2019-02-20 08:53:31 +0100

    update

--- Pendu/assets/js/script.js
+++ Pendu/assets/js/script.js
@@ -10,28 +10,32 @@ guessLetter();
 
 function guessLetter(){
 
-let  entree = prompt("Entree une lettre ou pendez vous !").toUpperCase();
+entree = prompt("Entree une lettre ou pendez vous !").toUpperCase();
 console.log(entree);
   for (let i = 0; i <= mot.length; i++) {
       if (entree===mot[i]) {
-        vide.splice(i, 1, entree);
-        finish.push(entree);
-        alert("Ouf ! t'a failli y passer, entre une nouvelle lettre")
+        finish[i]=mot[i]
+       // alert("Ouf ! t'a failli y passer, entre une nouvelle lettre")
         console.log(vide);
         console.log(mot);
         console.log(finish);
-            
-      }       
-  }
-  guessLetter();
-  fin();
-  function fin(){
-    finish=mot;
-      alert("Bravo, mais tu m'a trop pris la tête, on va te pendre quand même !" +finish);
+       }     
+      }
+       if(finish.length !== mot.length) {
+         guessLetter()
+       }
+       else{
+        return alert("Bravo, mais tu m'a trop pris la tête, on va te pendre quand même !" + finish);
 
-  }
+       }
   
 
+
+   
+  // else {
+  //    guessLetter();
+  // }
+ 
    
 }
 
