194b75fd6d81e4df76a4f1f6c5ebab5a633ae2af
faouzifouz <titahfaouzi@gmail.com>
2019-02-21 16:36:43 +0100

    update

---
+++ en selle/recall/questions.js
@@ -0,0 +1,145 @@
+var selectElementsStartingWithA = function(array) {
+    return array.filter((begin) => begin.startsWith("a"));
+}
+
+var selectElementsStartingWithVowel = function(array) {
+    
+    return array.filter((begin) => begin.startsWith("a")
+     ||begin.startsWith("e") 
+     ||begin.startsWith ("i") 
+     ||begin.startsWith ("o") 
+     ||begin.startsWith("u") 
+     ||begin.startsWith("y"))
+}
+
+var removeNullElements = function(array) {
+    return array.filter((bolean)=>bolean !== null)  ;
+}
+
+var removeNullAndFalseElements = function(array) {
+    return array.filter((bolean) =>bolean !== false).filter((element)=>element!==null)
+}
+
+var reverseWordsInArray = function(array) {
+    let test = [];
+    for (let i = 0; i< array.length; i++) {
+        test.push(array[i].split('').reverse().join(''));
+    }
+    return test;
+}
+
+var everyPossiblePair = function(array) {
+    return 'Write your method here';
+}
+
+var allElementsExceptFirstThree = function(array) {
+    return 'Write your method here';
+}
+
+var addElementToBeginning = function(array, element) {
+    return 'Write your method here';
+}
+
+var sortByLastLetter = function(array) {
+    return 'Write your method here';
+}
+
+var getFirstHalf = function(string) {
+    return 'Write your method here';
+}
+
+var makeNegative = function(number) {
+    return 'Write your method here';
+}
+
+var numberOfPalindromes = function(array) {
+    return 'Write your method here';
+}
+
+var shortestWord = function(array) {
+    return 'Write your method here';
+}
+
+var longestWord = function(array) {
+    return 'Write your method here';
+}
+
+var sumNumbers = function(array) {
+    return 'Write your method here';
+}
+
+var repeatElements = function(array) {
+    return 'Write your method here';
+}
+
+var stringToNumber = function(string) {
+    return 'Write your method here';
+}
+
+var calculateAverage = function(array) {
+    return 'Write your method here';
+}
+
+var getElementsUntilGreaterThanFive = function(array) {
+    return 'Write your method here';
+}
+
+var convertArrayToObject = function(array) {
+    return 'Write your method here';
+}
+
+var getAllLetters = function(array) {
+    return 'Write your method here';
+}
+
+var swapKeysAndValues = function(object) {
+    return 'Write your method here';
+}
+
+var sumKeysAndValues = function(object) {
+    return 'Write your method here';
+}
+
+var removeCapitals = function(string) {
+    return 'Write your method here';
+}
+
+var roundUp = function(number) {
+    return 'Write your method here';
+}
+
+var formatDateNicely = function(date) {
+    return 'Write your method here';
+}
+
+var getDomainName = function(string) {
+    return 'Write your method here';
+}
+
+var titleize = function(string) {
+    return 'Write your method here';
+}
+
+var checkForSpecialCharacters = function(string) {
+    return 'Write your method here';
+}
+
+var squareRoot = function(number) {
+    return 'Write your method here';
+}
+
+var factorial = function(number) {
+    return 'Write your method here';
+}
+
+var findAnagrams = function(string) {
+    return 'Write your method here';
+}
+
+var convertToCelsius = function(number) {
+    return 'Write your method here';
+}
+
+var letterPosition = function(array) {
+    return 'Write your method here';
+}
